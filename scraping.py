0b99c5296787183df7bedf0fd047cb3e91614e17
Lubi Voroga <lubi@macbook-air.lan>
2020-08-30 22:53:38 -0400

    updating scaping.py file

--- scraping.py
+++ scraping.py
@@ -17,7 +17,8 @@ def scrape_all():
         "news_paragraph": news_paragraph,
         "featured_image": featured_image(browser),
         "facts": mars_facts(),
-        "last_modified": dt.datetime.now()
+        "last_modified": dt.datetime.now(),
+        "mars_hemispheres": mars_hemispheres(browser)
     }
 
     # Stop webdriver and return data
@@ -105,6 +106,59 @@ def mars_facts():
 
     # Convert dataframe into HTML format, add bootstrap
     return df.to_html()
+
+def mars_hemispheres(browser):
+ # # D1: Scrape High-Resolution Mars’ Hemisphere Images and Titles
+    # ### Hemispheres
+    # 1. Use browser to visit the URL 
+    url = 'https://astrogeology.usgs.gov/search/results?q=hemisphere+enhanced&k1=target&v1=Mars'
+    browser.visit(url)
+
+    # Optional delay for loading the page
+    browser.is_element_present_by_css("ul.item_list li.slide", wait_time=1)
+
+    # 2. Create a list to hold the images and titles.
+    hemisphere_image_urls = []
+
+    # Parse the data
+    html = browser.html
+    hemisphere_soup = soup(html, 'html.parser')
+    hemisphere_image_urls = hemisphere_soup.find_all('div', class_=['description'])
+
+
+    # 3. Write code to retrieve the image urls and titles for each hemisphere.
+    cnt=0 
+    for hemisphere_image_url in hemisphere_image_urls:
+        try:
+            # Find the relative image url 
+            img_url_rel = hemisphere_image_url.a['href']
+            # Use the base url to create an absolute url
+            img_url = f'https://astrogeology.usgs.gov{img_url_rel}'
+
+            browser.visit(img_url)
+
+            # Optional delay for loading the page
+            browser.is_element_present_by_css("ul.item_list li.slide", wait_time=1)
+
+             # Parse the resulting html with soup
+            html = browser.html
+            img_soup = soup(html, 'html.parser')
+       
+            hemispheres = {}
+            img_url = img_soup.find('div', class_=['downloads']).a['href']
+            title = img_soup.find('h2').get_text()
+
+            hemispheres = {'img_url': img_url, 'title': title.get_text()}
+            hemisphere_image_urls.append(hemispheres)
+            cnt+=1
+            
+        except AttributeError:
+            return None
+
+        
+
+# 4. Print the list that holds the dictionary of each image url and title
+    return hemisphere_image_urls   
 
 # Stop webdriver and return data
 #    browser.quit()
